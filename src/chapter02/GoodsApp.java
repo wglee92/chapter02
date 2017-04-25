82a5d4bca6b79e2448826515793348c9cc4eadd6
bit-user <bit-user@bit>
2017-04-25 21:31:08 +0900

    test commit

--- src/chapter02/GoodsApp.java
+++ src/chapter02/GoodsApp.java
@@ -17,7 +17,16 @@ public class GoodsApp {
 		Goods goods1 = new Goods();
 		Goods goods2 = new Goods();
 		Goods goods3 = new Goods();
-
+		
+		
 		System.out.println(Goods.countOfGoods);
+
+		double i = 3 * Math.PI;
+		
+		int a = 10;
+		a = 20;
+		
+		final int B =20; //상수(값대입이 여기가 끝. 모두다 대문자로 표현하는 관례)
+		// B = 30; // 상수를 변경못하므로 에러
 	}
 }
